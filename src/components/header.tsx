cf974764e8b7ec64f91cef7f92d8a37b6f4381b0
huangjoseph45 <huangjoseph45@gmail.com>
2025-08-29 13:53:22 -0400

    fix header spacing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./themetoggle";
 
 const Header: React.FC = () => {
   return (
-    <div className="z-50 w-full left-full top-0 h-fit px-12 py-4 flex justify-end">
+    <div className="z-50 w-full left-full top-0 h-fit px-4 md:px-12 py-4 flex justify-end ">
       <ThemeToggle />
     </div>
   );
