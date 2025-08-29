29d8a88617e9222b1df0f5b4e659615057551edb
huangjoseph45 <huangjoseph45@gmail.com>
2025-08-29 11:14:43 -0400

    fix to themetoggle loc

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./themetoggle";
 
 const Header: React.FC = () => {
   return (
-    <div className="z-50 w-full left-full top-0 h-fit p-2 flex justify-end mb-4">
+    <div className="z-50 w-full left-full top-0 h-fit px-12 py-4 flex justify-end">
       <ThemeToggle />
     </div>
   );
